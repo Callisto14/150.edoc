43abc10e82c00b5cba090ffa550ac5537e9b8b1a
Callisto14 <89420339+Callisto14@users.noreply.github.com>
2023-02-20 10:49:07 +0300

    Add files via upload

---
+++ #018.py
@@ -0,0 +1,22 @@
+"""
+Ask the user to enter a number. If it is under 10, 
+display the message “Too low”, if their number is 
+between 10 and 20, display “Correct”, otherwise 
+display “Too high”.
+"""
+
+Num = ("Enter a number: ")
+
+if Num < 10:
+    print("Too low")
+elif Num 10 or <= 20:
+    print("Correct")
+else:
+    print("Too high")
+
+
+
+input()
+
+
+#took 1 try! :)
